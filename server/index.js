d0706ca560ef7183f9de03360711e4d82748c75c
Ethan Liang <6923102@qq.com>
2017-10-21 12:38:08 +0800

    deps

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ const sendToOther = (selfCennect, data) => {
   })
 }
 wss.on('connection', function connection(ws) {
-  const location = url.parse(ws.upgradeReq.url, true);
+  // const location = url.parse(ws.upgradeReq.url, true);
   // You might use location.query.access_token to authenticate or share sessions
   // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
   allConnect.push(ws)
